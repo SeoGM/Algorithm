4a2b65aa315ef3349e2d98e2992100156884c1e2
SeoGM <auddlzz000@gmail.com>
2024-06-04 17:23:18 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.js
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -1,6 +1,3 @@
 const solution = (angle) => {
-    return angle === 180 ? 4 : 
-           angle < 180 && angle > 90 ? 3 : 
-           angle === 90 ? 2 : 
-           angle < 90 && angle > 0 ? 1 : 0;
+    return [0, 90, 91, 180].filter(x => angle>=x).length;
 }
