b834531d887d7ea74cebe3b42cf9dc4e09cde675
SeoGM <auddlzz000@gmail.com>
2024-06-05 13:37:16 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.js
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -1,9 +1,4 @@
 function solution(money) {
     const americano = 5500;
-    
-    const getCoffee = Math.floor(money / americano);
-    const change = money - (americano * getCoffee);
-    
-    var answer = [getCoffee, change];
-    return answer;
+    return [Math.floor(money / americano), money % americano];
 }
