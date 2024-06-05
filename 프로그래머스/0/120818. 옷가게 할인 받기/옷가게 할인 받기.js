c414b6ad29988e30b0465ff3c38e11ef5a3b9103
SeoGM <auddlzz000@gmail.com>
2024-06-05 13:26:01 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,14 @@
+function solution(price) {
+    let discount = 0;
+
+    if (price >= 500000) {
+        discount = 0.20;
+    } else if (price >= 300000) {
+        discount = 0.10;
+    } else if (price >= 100000) {
+        discount = 0.05;
+    }
+
+    const finalPrice = Math.trunc(price * (1 - discount));
+    return finalPrice;
+}
