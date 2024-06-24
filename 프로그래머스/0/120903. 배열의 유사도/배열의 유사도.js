6a45c5b79b3da9ed1aa58578a9a4b4d5136d51d2
SeoGM <auddlzz000@gmail.com>
2024-06-24 13:55:51 +0900

    [level 0] Title: 배열의 유사도, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,8 +1,3 @@
 function solution(s1, s2) {
-    let answer = 0;
-    s1.map((v1) => {
-        s2.map(v2=>v1===v2&&answer++)
-    })
-    
-    return answer;
+    return s1.filter(v=>s2.includes(v)).length;
 }
