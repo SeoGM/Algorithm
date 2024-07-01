64bceb2c3eb5475872a9347a4e543ffdc9d7f485
SeoGM <auddlzz000@gmail.com>
2024-07-01 22:34:31 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -1,4 +1,3 @@
 function solution(my_string) {
-    const reg = /[^0-9]/g;
-    return my_string.replace(reg, "").split("").map(Number).sort((a,b)=>a-b);
+    return my_string.match(/\d/g).sort((a, b) => a - b).map(Number);
 }
