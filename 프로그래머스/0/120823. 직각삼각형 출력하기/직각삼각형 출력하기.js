2fee5001dafaa00ff5e202bd0bf34b83dd5eb0b0
SeoGM <auddlzz000@gmail.com>
2024-07-02 14:16:43 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 59.09 ms, Memory: 32.4 MB -BaekjoonHub

--- 프로그래머스/0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.js
+++ 프로그래머스/0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.js
@@ -9,12 +9,8 @@ let output = "";
 rl.on('line', function (line) {
     input = line.split(' ');
 }).on('close', function () {
-    for (let i = 0; i < Number(input[0]); i++) {
-        for ( let j = 0; j <= i; j++) {
-            output += "*";
-        }
-        output += "\n";
+    for (let i = 1; i <= Number(input[0]); i++) {
+        console.log('*'.repeat(i));
     }
-    console.log(output)
 });
 
